e65429333937b02c0c00eeb2d8b44435df95d31a
Yehuda Bortz <yehudabortz@icloud.com>
2021-03-14 20:09:17 -0500

    commit tree efcdbf4f3c8c61dba53b24422f0d57ca9587fddf

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your code here!
+let element = document.createElement('div')
+
+document.body.appendChild(element)
+let ul = document.createElement('ul')
+let h = document.createElement('h1')
+
+for (let i = 0; i < 100; i++) {
+    let li = document.createElement('li')
+    li.innerHTML = (i+1).toString()
+    ul.appendChild(li)
+}
+
+h.innerHTML = "Yehuda"
+element.appendChild(ul)
+
+
+
+ul.removeChild(ul.querySelector('li:nth-child(5)'))
+ul.remove()
+
+main = document.querySelector('main#main')
+main.remove()
+
+
+let newHeader = document.createElement('h1')
+newHeader.setAttribute('id', 'victory')
+newHeader.innerHTML = "Yehuda is the champion"
+document.body.appendChild(newHeader)
